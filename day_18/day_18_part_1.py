3445b2fd98018d1a0effbd3678b7b68ae5ba8723
medrzec <fmynarski@gmail.com>
2017-12-21 12:10:43 +0100

    day solutions

---
+++ day_18/day_18_part_1.py
@@ -0,0 +1,64 @@
+puzzle = open('puzzle').read().splitlines()
+puzzle = [i.split() for i in puzzle]
+
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+registers = {str(i[1]): 0 for i in puzzle}
+idx = 0
+save = 0
+
+while idx < len(puzzle):
+	i = puzzle[idx]
+	if puzzle[idx][0] == 'snd':
+		if RepresentsInt(i[1]):
+			save = int(i[1])
+		else:
+			save = registers[i[1]]
+
+	elif puzzle[idx][0] == 'set':
+		if RepresentsInt(i[2]):
+			registers[i[1]] = int(i[2])
+		else:
+			registers[i[1]] = registers[i[2]]
+
+	elif puzzle[idx][0] == 'add':
+		if RepresentsInt(i[2]):
+			registers[i[1]] += int(i[2])
+		else:
+			registers[i[1]] += registers[i[2]]
+
+	elif puzzle[idx][0] == 'mul':
+		if RepresentsInt(i[2]):
+			registers[i[1]] *= int(i[2])
+		else:
+			registers[i[1]] *= registers[i[2]]
+
+	elif puzzle[idx][0] == 'mod':
+		if RepresentsInt(i[2]):
+			registers[i[1]] %= int(i[2])
+		else:
+			registers[i[1]] %= registers[i[2]]
+
+	elif puzzle[idx][0] == 'rcv':
+		if save != 0:
+			print save
+			break
+
+	elif puzzle[idx][0] == 'jgz':
+		if RepresentsInt(i[1]) and int(i[1]) > 0:
+			if RepresentsInt(i[2]):
+				idx += int(i[2])
+			else:
+				idx += registers[i[2]]
+		elif registers[i[1]] > 0:
+			if RepresentsInt(i[2]):
+				idx += int(i[2]) - 1
+			else:
+				idx += registers[i[2]] - 1
+
+	idx += 1
